a6e6b6737f514f9405cf48c95df53a135c61e0b8
Chiao-Han Chang <mandy93142@gmail.com>
2024-02-06 13:26:37 +0800

    finished category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,15 +37,45 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
+  try {
+    const newcategoryData = await Category.create(req.body);
+    res.status(200).json(newcategoryData);
+  } catch(err) {
+    res.status(400).json(err);
+  };
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(400).json(err);
+  };
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(categoryData) {
+      res.status(200).json({ status: `deleted category id = ${req.params.id}`})
+    } else {
+      res.status(400).json({ status: `no category of id = ${req.params.id}`})
+    }
+   
+
+
+  } catch(err) {
+    res.status(400).json(err);
+  };
 });
 
 module.exports = router;
